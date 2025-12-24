182d967414eb74330facbe0cfcdc30c6791b7745
Aurickk <68048847+aurickk@users.noreply.github.com>
2025-12-24 14:56:22 -0500

    Removed commands

--- src/client/java/incognito/mod/IncognitoClient.java
+++ src/client/java/incognito/mod/IncognitoClient.java
@@ -1,15 +1,12 @@
 package incognito.mod;
 
-import incognito.mod.command.IncognitoCommand;
 import incognito.mod.config.IncognitoConfig;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 
 public class IncognitoClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		IncognitoConfig.getInstance();
-		ClientCommandRegistrationCallback.EVENT.register(IncognitoCommand::register);
 		Incognito.LOGGER.info("Incognito client protection initialized");
 	}
 }
